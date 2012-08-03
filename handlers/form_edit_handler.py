bc03c99a63ab237e7f9072cc78608870bd07f4e4
Emily Anderson <emily@eandersons.net>
2012-08-03 14:28:56 -0700

    Require login to use, and link categories to users

--- handlers/form_edit_handler.py
+++ handlers/form_edit_handler.py
@@ -6,4 +6,4 @@ from utils.jinja_env import JinjaEnv
 class FormEditHandler(webapp2.RequestHandler):
     def get(self):
         template = JinjaEnv.get().get_template('templates/form_edit_form.html')
-        self.response.out.write(template.render({'categories': EvalCategory.list()}))
+        self.response.out.write(template.render({'categories': EvalCategory.list_by_user()}))
--- models/eval_category.py
+++ models/eval_category.py
@@ -1,9 +1,11 @@
 from google.appengine.ext import db
+from google.appengine.api import users
 
 from models.base import BaseModel
 
 class EvalCategory(BaseModel):
     name = db.StringProperty()
+    owner_user_id = db.StringProperty()
     created = db.DateTimeProperty(auto_now_add=True)
     modified = db.DateTimeProperty(auto_now=True)
     
@@ -11,7 +13,15 @@ class EvalCategory(BaseModel):
     def create(self, name):
         category = EvalCategory(parent=self.report_key())
         category.name = name
+        category.owner_user_id = users.get_current_user().user_id()
         return category.put()
+    
+    @classmethod
+    def list_by_user(self, user=None):
+        if not user:
+          user = users.get_current_user()
+        categories = self.gql("WHERE owner_user_id = :1", user.user_id()).fetch(100)
+        return categories
     
     def items(self):
         from models.eval_item import EvalItem
