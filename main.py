b51ae4706212fbd2f5db26280c1ea8a57d655377
Emily Anderson <emily@eandersons.net>
2012-08-15 09:23:47 -0700

    Be able to delete categories and items

--- main.py
+++ main.py
@@ -27,12 +27,16 @@ from handlers.category_add_form_handler import CategoryAddFormHandler
 from handlers.category_add_handler import CategoryAddHandler
 from handlers.category_edit_form_handler import CategoryEditFormHandler
 from handlers.category_edit_handler import CategoryEditHandler
+from handlers.category_delete_form_handler import CategoryDeleteFormHandler
+from handlers.category_delete_handler import CategoryDeleteHandler
 from handlers.category_move_up_handler import CategoryMoveUpHandler
 from handlers.category_move_down_handler import CategoryMoveDownHandler
 from handlers.item_add_form_handler import ItemAddFormHandler
 from handlers.item_add_handler import ItemAddHandler
 from handlers.item_edit_form_handler import ItemEditFormHandler
 from handlers.item_edit_handler import ItemEditHandler
+from handlers.item_delete_form_handler import ItemDeleteFormHandler
+from handlers.item_delete_handler import ItemDeleteHandler
 from handlers.item_move_up_handler import ItemMoveUpHandler
 from handlers.item_move_down_handler import ItemMoveDownHandler
 from utils.jinja_env import JinjaEnv
@@ -52,10 +56,14 @@ app = webapp2.WSGIApplication([
     webapp2.Route(r'/card/<card_id:\d+>/category/add', handler=CategoryAddHandler),
     webapp2.Route(r'/item/<item_id:\d+>/edit_form', handler=ItemEditFormHandler),
     webapp2.Route(r'/item/<item_id:\d+>/edit', handler=ItemEditHandler),
+    webapp2.Route(r'/item/<item_id:\d+>/delete_form', handler=ItemDeleteFormHandler),
+    webapp2.Route(r'/item/<item_id:\d+>/delete', handler=ItemDeleteHandler),
     webapp2.Route(r'/item/<item_id:\d+>/move_up', handler=ItemMoveUpHandler),
     webapp2.Route(r'/item/<item_id:\d+>/move_down', handler=ItemMoveDownHandler),
     webapp2.Route(r'/category/<category_id:\d+>/edit_form', handler=CategoryEditFormHandler),
     webapp2.Route(r'/category/<category_id:\d+>/edit', handler=CategoryEditHandler),
+    webapp2.Route(r'/category/<category_id:\d+>/delete_form', handler=CategoryDeleteFormHandler),
+    webapp2.Route(r'/category/<category_id:\d+>/delete', handler=CategoryDeleteHandler),
     webapp2.Route(r'/category/<category_id:\d+>/move_up', handler=CategoryMoveUpHandler),
     webapp2.Route(r'/category/<category_id:\d+>/move_down', handler=CategoryMoveDownHandler),
     webapp2.Route(r'/category/<category_id:\d+>/item/add_form', handler=ItemAddFormHandler),
