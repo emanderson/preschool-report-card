67cf0724c651e2b6ec8a9f7b6b746cc706a92cbd
Emily Anderson <emily@eandersons.net>
2012-08-24 09:54:12 -0700

    Condense overly verbose selenium clicks, add move up/down tests for text lines

--- selenium_tests/card_basics_test.py
+++ selenium_tests/card_basics_test.py
@@ -6,30 +6,24 @@ class CardBasicsTest(SeleniumTestCase):
         self.login_user('cardbasics%d@example.com' % time.time())
         
         # Add a new card
-        add_button = self.driver.find_element_by_id('addCard')
-        add_button.click()
+        self.driver.find_element_by_id('addCard').click()
         name_field = self.wait_for_id('name')
         name_field.send_keys('New Card')
-        save_button = self.driver.find_element_by_id('add')
-        save_button.click()
+        self.driver.find_element_by_id('add').click()
         
         # Go to edit page
-        edit_button = self.driver.find_elements_by_class_name('editCard')[0]
-        edit_button.click()
+        self.driver.find_elements_by_class_name('editCard')[0].click()
         
         # Add to key
-        add_key_button = self.driver.find_element_by_id('addKey')
-        add_key_button.click()
+        self.driver.find_element_by_id('addKey').click()
         name_field = self.wait_for_id('name')
         name_field.send_keys('New Key')
         score_field = self.driver.find_element_by_id('score')
         score_field.send_keys('5')
-        save_button = self.driver.find_element_by_id('add')
-        save_button.click()
+        self.driver.find_element_by_id('add').click()
         
         # Edit key
-        edit_key_button = self.driver.find_elements_by_class_name('editKey')[0]
-        edit_key_button.click()
+        self.driver.find_elements_by_class_name('editKey')[0].click()
         name_field = self.wait_for_id('name')
         # TODO: check contents?
         name_field.clear()
@@ -37,38 +31,39 @@ class CardBasicsTest(SeleniumTestCase):
         score_field = self.driver.find_element_by_id('score')
         score_field.clear()
         score_field.send_keys('2')
-        save_button = self.driver.find_element_by_id('edit')
-        save_button.click()
+        self.driver.find_element_by_id('edit').click()
         
         # Delete key
-        delete_key_button = self.driver.find_elements_by_class_name('deleteKey')[0]
-        delete_key_button.click()
-        delete_button = self.wait_for_id('delete')
-        delete_button.click()
+        self.driver.find_elements_by_class_name('deleteKey')[0].click()
+        self.wait_for_id('delete').click()
         
-        # Add text line
-        add_text_button = self.driver.find_element_by_id('addText')
-        add_text_button.click()
+        # Add text lines
+        self.driver.find_element_by_id('addText').click()
         name_field = self.wait_for_id('name')
-        name_field.send_keys('New Text Line')
-        save_button = self.driver.find_element_by_id('add')
-        save_button.click()
+        name_field.send_keys('First Text Line')
+        self.driver.find_element_by_id('add').click()
+        self.driver.find_element_by_id('addText').click()
+        name_field = self.wait_for_id('name')
+        name_field.send_keys('Second Text Line')
+        self.driver.find_element_by_id('add').click()
+        
+        # Move up/down
+        self.driver.find_elements_by_class_name('moveUpText')[1].click()
+        self.driver.find_elements_by_class_name('moveDownText')[0].click()
         
         # Edit text line
-        edit_text_button = self.driver.find_elements_by_class_name('editText')[0]
-        edit_text_button.click()
+        self.driver.find_elements_by_class_name('editText')[0].click()
         name_field = self.wait_for_id('name')
         # TODO: check contents?
         name_field.clear()
         name_field.send_keys('Updated Text')
-        save_button = self.driver.find_element_by_id('edit')
-        save_button.click()
+        self.driver.find_element_by_id('edit').click()
         
-        # Delete text line
-        delete_text_button = self.driver.find_elements_by_class_name('deleteText')[0]
-        delete_text_button.click()
-        delete_button = self.wait_for_id('delete')
-        delete_button.click()
+        # Delete text lines
+        self.driver.find_elements_by_class_name('deleteText')[0].click()
+        self.wait_for_id('delete').click()
+        self.driver.find_elements_by_class_name('deleteText')[0].click()
+        self.wait_for_id('delete').click()
         
         time.sleep(3)
         
