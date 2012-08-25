66a88e6a3846381d207a60359dabb442a2f04224
Emily Anderson <emily@eandersons.net>
2012-08-24 18:10:24 -0700

    Mask behind pop-ups so you can't click other controls

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
         $.ajax({
             url: this.href,
             success: function(data) {
+                $("body").append("<div class='mask'></div>");
                 $("body").append(data);
                 $(".formPopup").find(".closer").click(function() {
+                    $(".mask").remove();
                     $(".formPopup").remove();
                     return false;
                 });
@@ -18,8 +20,10 @@ $(document).ready(function() {
         $.ajax({
             url: $(this).closest('form')[0].action,
             success: function(data) {
+                $("body").append("<div class='mask'></div>");
                 $("body").append(data);
                 $(".formPopup").find(".closer").click(function() {
+                    $(".mask").remove();
                     $(".formPopup").remove();
                     return false;
                 });
